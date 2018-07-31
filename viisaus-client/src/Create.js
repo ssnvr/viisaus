0e4ebfa8b8d907b6f146fdf02888f579b3b7e1c8
ElliMaija <elli.salo@outlook.com>
2018-07-31 13:05:33 +0300

    Create, Post ja Login lisätty

---
+++ viisaus-client/src/Create.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Create extends Component {
+
+    
+
+    render() {
+        return (
+            <div>
+                <h5>
+                    Täydennä alle:
+                </h5>
+                <form>
+                    Käyttäjänimi:  
+                </form>
+
+
+            </div>
+
+        );
+    }
+}
---
+++ viisaus-client/src/Post.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+//import Vote from './Vote';
+
+class Post extends Component{
+    state={counter: '' }
+       
+    render(){
+        const{
+            nickname,
+            posttext
+
+        } = this.props;
+
+        return(
+            <div>
+                <p>{nickname}</p>
+                <p>{posttext}</p>
+                <Vote/>
+                <p>Tykkäykset: {this.state.counter}</p>
+                </div>
+        );
+
+    }
+}
+export default Post;
