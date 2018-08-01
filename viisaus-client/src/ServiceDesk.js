5ce2edd71781d3b3769c8d54d03ec13f68a215ae
ElliMaija <elli.salo@outlook.com>
2018-08-01 10:22:49 +0300

    createen lisätty databaseyhteyttä

--- viisaus-client/src/ServiceDesk.js
+++ viisaus-client/src/ServiceDesk.js
@@ -29,3 +29,32 @@ export function addNewMessage(msg, callback){
      callback(response.status);
    });
  }
+ //Tässä samat käyttäjille
+ export function getUsers(callback) {
+    fetch( "api/users")
+        .then(function (response) {
+            if (!response.ok) {
+                const errmsg = {
+                    status: response.status,
+                    statusText: response.statusText,
+                    msg: "Käyttäjienhaku"
+                };
+                throw errmsg;
+            }
+            return response.json();
+        })
+        .then(function (user) {
+            callback(user);
+        });
+}
+
+export function addNewUser(msg, callback){
+     fetch( "api/users/",{
+     method: 'POST',
+     headers: {'Content-Type': 'application/json'},
+     body: JSON.stringify(msg)
+   })
+   .then(function (response){
+     callback(response.status);
+   });
+ }
