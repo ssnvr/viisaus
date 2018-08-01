180779769180c15d1a7dcb49b6a0492a969f6a7c
annikaivola <anni.kaivola@gmail.com>
2018-08-01 09:33:51 +0300

    messagen fiksausyritys, mutta ei toimi vieläkään

--- viisaus-client/src/ServiceDesk.js
+++ viisaus-client/src/ServiceDesk.js
@@ -1,8 +1,8 @@
 //tein tämän tiedoston koska en osaa hahmottaa kun kaikki on app.js 
-const baseurl ='http://localhost:58232/';
+
 
 export function getMessages(callback) {
-    fetch(baseurl + "api/posts")
+    fetch( "api/posts")
         .then(function (response) {
             if (!response.ok) {
                 const errmsg = {
@@ -20,7 +20,7 @@ export function getMessages(callback) {
 }
 
 export function addNewMessage(msg, callback){
-    return fetch( baseurl +"api/posts/",{
+     fetch( "api/posts/",{
      method: 'POST',
      headers: {'Content-Type': 'application/json'},
      body: JSON.stringify(msg)
