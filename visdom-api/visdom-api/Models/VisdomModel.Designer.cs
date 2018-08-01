8562585206ae2cb81ee42926d2e3283d39d4c80d
annikaivola <anni.kaivola@gmail.com>
2018-08-01 13:53:25 +0300

    en tiedä mikä toimii ja mikä ei

--- visdom-api/visdom-api/Models/VisdomModel.Designer.cs
+++ visdom-api/visdom-api/Models/VisdomModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\stuki\source\repos\viisaus\visdom-api\visdom-api\Models\VisdomModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\work\Viikko8-Miniprojekti\viisaus\visdom-api\visdom-api\Models\VisdomModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
