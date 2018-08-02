1055d359707821538a21bb1310b59a4b312bea84
annikaivola <anni.kaivola@gmail.com>
2018-08-02 08:53:11 +0300

    jotain muutin

--- viisaus-client/src/Components/Message.js
+++ viisaus-client/src/Components/Message.js
@@ -50,7 +50,7 @@ class Message extends Component {
         console.log(this.state);
         return (
             <div className="message">
-                <textarea className="tekstibox" rows="1" cols="35" placeholder="Write your message here!" value={this.state.Message} onChange={this.messageCreated} />
+                <textarea className="tekstibox" rows="1" cols="35" placeholder="Write your message here!" value={this.state.Message} onChange={this.messageCreated} /><br/>
                 <input className="submitMessage" type="submit" value="Send message" onClick={this.sendMessage} />
             </div>
         )
